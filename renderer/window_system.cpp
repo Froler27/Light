a62414246a6d9f2d34764b746ea4b68141dd3aba
Froler27 <wqpyihu@163.com>
2023-09-01 13:18:01 +0800

    add window system

---
+++ renderer/window_system.cpp
@@ -0,0 +1,75 @@
+#include "window_system.h"
+#include <iostream>
+
+namespace Light
+{
+    WindowSystem::~WindowSystem()
+    {
+        glfwDestroyWindow(m_window);
+        glfwTerminate();
+    }
+
+    void WindowSystem::initialize(WindowCreateInfo create_info)
+    {
+        if (!glfwInit())
+        {
+            std::cout << __FUNCTION__ << " failed to initialize GLFW" << std::endl;
+            return;
+        }
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        m_window = glfwCreateWindow(create_info.width, create_info.height, create_info.title, NULL, NULL);
+        if (m_window == NULL)
+        {
+            std::cout << __FUNCTION__ << " failed to create window" << std::endl;
+            glfwTerminate();
+            return;
+        }
+
+        glfwMakeContextCurrent(m_window);
+
+        // Setup input callbacks
+        glfwSetWindowUserPointer(m_window, this);
+        glfwSetKeyCallback(m_window, keyCallback);
+        glfwSetCharCallback(m_window, charCallback);
+        glfwSetCharModsCallback(m_window, charModsCallback);
+        glfwSetMouseButtonCallback(m_window, mouseButtonCallback);
+        glfwSetCursorPosCallback(m_window, cursorPosCallback);
+        glfwSetCursorEnterCallback(m_window, cursorEnterCallback);
+        glfwSetScrollCallback(m_window, scrollCallback);
+        glfwSetDropCallback(m_window, dropCallback);
+        glfwSetWindowSizeCallback(m_window, windowSizeCallback);
+        glfwSetWindowCloseCallback(m_window, windowCloseCallback);
+
+        glfwSetInputMode(m_window, GLFW_RAW_MOUSE_MOTION, GLFW_FALSE);
+    }
+
+    void WindowSystem::pollEvents() const { glfwPollEvents(); }
+
+    void WindowSystem::swapBuffers() const
+    {
+        glfwSwapBuffers(m_window);
+    }
+
+    bool WindowSystem::shouldClose() const { return glfwWindowShouldClose(m_window); }
+
+    void WindowSystem::setTitle(const char* title) { glfwSetWindowTitle(m_window, title); }
+
+    GLFWwindow* WindowSystem::getWindow() const { return m_window; }
+
+    std::array<int, 2> WindowSystem::getWindowSize() const { return std::array<int, 2>({m_width, m_height}); }
+
+    void WindowSystem::setFocusMode(bool mode)
+    {
+        m_is_focus_mode = mode;
+        glfwSetInputMode(m_window, GLFW_CURSOR, m_is_focus_mode ? GLFW_CURSOR_DISABLED : GLFW_CURSOR_NORMAL);
+    }
+
+    void WindowSystem::setShouldClose(bool close)
+    {
+        glfwSetWindowShouldClose(m_window, close);
+    }
+
+}
--- test/main.cpp
+++ test/main.cpp
@@ -1,52 +1,26 @@
-// #include <iostream>
-// #include <editor.h>
-// #include <test.h>
-// 
-// int main()
-// {
-//     Editor::Editor editor;
-//     editor.initialize();
-//     editor.run();
-//     return 0;
-// }
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <shader.h>
 
 #include <iostream>
+#include <memory>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+#include <window_system.h>
+
+void framebuffer_size_callback(int width, int height);
 void processInput(GLFWwindow* window);
+void handleKeyEvent(int key, int scancode, int action, int mods);
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+std::unique_ptr<Light::WindowSystem> windowSystem;
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    windowSystem = std::make_unique<Light::WindowSystem>();
+    windowSystem->initialize(Light::WindowCreateInfo());
+    
+    windowSystem->registerOnWindowSizeFunc(framebuffer_size_callback);
+    windowSystem->registerOnKeyFunc(handleKeyEvent);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
@@ -92,12 +66,8 @@ int main()
 
     // render loop
     // -----------
-    while (!glfwWindowShouldClose(window))
+    while (!windowSystem->shouldClose())
     {
-        // input
-        // -----
-        processInput(window);
-
         // render
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -108,10 +78,8 @@ int main()
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        windowSystem->swapBuffers();
+        windowSystem->pollEvents();
     }
 
     // optional: de-allocate all resources once they've outlived their purpose:
@@ -119,9 +87,6 @@ int main()
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    glfwTerminate();
     return 0;
 }
 
@@ -135,9 +100,22 @@ void processInput(GLFWwindow* window)
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+void handleKeyEvent(int key, int scancode, int action, int mods)
+{
+    switch (key) {
+    case GLFW_KEY_ESCAPE:
+        if (action == GLFW_PRESS) {
+            windowSystem->setShouldClose(true);
+        }
+        break;
+    default:
+        break;
+    }
 }
