9ab01fa4c19ef560bd9c76b374b2d4b7304758b0
Froler27 <wqpyihu@163.com>
2023-09-01 13:18:01 +0800

    add window system

---
+++ link.bat
@@ -0,0 +1,3 @@
+set current_dir=%~dp0
+mklink /D %current_dir%\build\test\resources\ %current_dir%\resources\
+pause
--- test/main.cpp
+++ test/main.cpp
@@ -1,52 +1,26 @@
-// #include <iostream>
-// #include <editor.h>
-// #include <test.h>
-// 
-// int main()
-// {
-//     Editor::Editor editor;
-//     editor.initialize();
-//     editor.run();
-//     return 0;
-// }
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <shader.h>
 
 #include <iostream>
+#include <memory>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+#include <window_system.h>
+
+void framebuffer_size_callback(int width, int height);
 void processInput(GLFWwindow* window);
+void handleKeyEvent(int key, int scancode, int action, int mods);
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+std::unique_ptr<Light::WindowSystem> windowSystem;
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    windowSystem = std::make_unique<Light::WindowSystem>();
+    windowSystem->initialize(Light::WindowCreateInfo());
+    
+    windowSystem->registerOnWindowSizeFunc(framebuffer_size_callback);
+    windowSystem->registerOnKeyFunc(handleKeyEvent);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
@@ -58,7 +32,7 @@ int main()
 
     // build and compile our shader program
     // ------------------------------------
-    Shader ourShader("3.3.shader.vs", "3.3.shader.fs"); // you can name your shader files however you like
+    Shader ourShader("./resources/shaders/3.3.shader.vs", "./resources/shaders/3.3.shader.fs"); // you can name your shader files however you like
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -92,12 +66,8 @@ int main()
 
     // render loop
     // -----------
-    while (!glfwWindowShouldClose(window))
+    while (!windowSystem->shouldClose())
     {
-        // input
-        // -----
-        processInput(window);
-
         // render
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -108,10 +78,8 @@ int main()
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        windowSystem->swapBuffers();
+        windowSystem->pollEvents();
     }
 
     // optional: de-allocate all resources once they've outlived their purpose:
@@ -119,25 +87,27 @@ int main()
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    glfwTerminate();
     return 0;
-}
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+void handleKeyEvent(int key, int scancode, int action, int mods)
+{
+    switch (key) {
+    case GLFW_KEY_ESCAPE:
+        if (action == GLFW_PRESS) {
+            windowSystem->setShouldClose(true);
+        }
+        break;
+    default:
+        break;
+    }
 }
